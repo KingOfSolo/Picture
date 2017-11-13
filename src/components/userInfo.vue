d0cd3357397725a42bb170c0f2ebf4193d367b7a
KingOfSolo <944125893@qq.com>
2017-11-14 00:36:48 +0800

    user info edit

--- src/components/userInfo.vue
+++ src/components/userInfo.vue
@@ -1,47 +1,47 @@
 <template>
   <div id="user-info">
-    <form class="user-info-form" @mouseenter="formEnter" @mouseleave="formLeave">
+    <form class="user-info-form" @mouseenter="formEnter(0)" @mouseleave="formLeave(0)">
       <h3 class="form-label">昵称</h3>
       <div class="form-content">
         <div class="form-content-show">
           <span class="form-text">只为红颜</span>
-          <edit-button show-edit="showEdit"></edit-button>
+          <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[0] }">修改</el-button>
         </div>
       </div>
     </form>
-    <form class="user-info-form">
+    <form class="user-info-form" @mouseenter="formEnter(1)" @mouseleave="formLeave(1)">
       <h3 class="form-label">性别</h3>
       <div class="form-content">
         <span class="form-text">未知</span>
-        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit }">修改</el-button>
+        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[1] }">修改</el-button>
       </div>
     </form>
-    <form class="user-info-form">
+    <form class="user-info-form" @mouseenter="formEnter(2)" @mouseleave="formLeave(2)">
       <h3 class="form-label">头像</h3>
       <div class="form-content">
         <img style="width: 45px;height: 45px;margin-right: 15px" src="../assets/user.jpg"/>
-        <el-button type="text" icon="el-icon-edit">修改</el-button>
+        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[2] }">修改</el-button>
       </div>
     </form>
-    <form class="user-info-form">
+    <form class="user-info-form" @mouseenter="formEnter(3)" @mouseleave="formLeave(3)">
       <h3 class="form-label">密码</h3>
       <div class="form-content">
         <span class="form-text">******</span>
-        <el-button type="text" icon="el-icon-edit">修改</el-button>
+        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[3] }">修改</el-button>
       </div>
     </form>
-    <form class="user-info-form">
+    <form class="user-info-form" @mouseenter="formEnter(4)" @mouseleave="formLeave(4)">
       <h3 class="form-label">绑定手机</h3>
       <div class="form-content">
         <span class="form-text">未绑定</span>
-        <el-button type="text" icon="el-icon-edit">修改</el-button>
+        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[4] }">修改</el-button>
       </div>
     </form>
-    <form class="user-info-form">
+    <form class="user-info-form" @mouseenter="formEnter(5)" @mouseleave="formLeave(5)">
       <h3 class="form-label">个人简介</h3>
       <div class="form-content">
         <span class="form-text">世间所有的相遇 都是久别重逢</span>
-        <el-button type="text" icon="el-icon-edit">修改</el-button>
+        <el-button type="text" icon="el-icon-edit" v-bind:class="{ editHidden: showEdit[5] }">修改</el-button>
       </div>
     </form>
   </div>
@@ -62,17 +62,17 @@
       EditButton},
     data () {
       return {
-        showEdit: true
+        showEdit: [false, true, true, true, true, true]
       }
     },
     computed: {
     },
     methods: {
-      formEnter () {
-        this.showEdit = false
+      formEnter (index) {
+        this.showEdit[index] = false
       },
-      formLeave () {
-        this.showEdit = true
+      formLeave (index) {
+        this.showEdit[index] = true
       }
     }
   }
