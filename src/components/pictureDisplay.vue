d4050d821db41d690889e06f470e0ea2a22cab99
KingOfSolo <944125893@qq.com>
2017-11-15 22:22:14 +0800

    image mask

--- src/components/pictureDisplay.vue
+++ src/components/pictureDisplay.vue
@@ -1,24 +1,46 @@
 <template>
   <div id="picture-display">
-    <img src="../assets/picture.jpg" class="image"/>
+    <div class="post-item">
+      <div class="post-body">
+        <img src="../assets/picture.jpg" class="image"/>
+        <div class="image-mask">
+        </div>
+      </div>
+    </div>
     <div>
       <div id="picture-display-bottom">
         <img src="../assets/user.jpg"/>
         <span style="cursor: pointer">只为红颜</span>
         <div id="picture-display-button">
-          <span class="picture-icon" style="padding-right: 10px"><i class="fa fa-heart"></i><span class="picture-icon-num">0</span></span>
+          <span class="picture-icon" style="padding-right: 10px" v-bind:class="{redColor: isHeart}" @click="heartClick">
+            <i class="fa fa-heart"></i><span class="picture-icon-num">{{ heartNum }}</span></span>
           <span class="picture-icon"><i class="fa fa-comments"></i><span class="picture-icon-num">0</span></span>
         </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
   export default{
     data () {
       return {
-        currentDate: new Date()
+        currentDate: new Date(),
+        showMask: false,
+        isHeart: false,
+        heartNum: 0
+      }
+    },
+    methods: {
+      heartClick: function () {
+        if (this.isHeart) {
+          this.isHeart = !this.isHeart
+          this.heartNum -= 1
+        } else {
+          this.isHeart = !this.isHeart
+          this.heartNum += 1
+        }
       }
     }
   }
@@ -27,31 +49,82 @@
 <style>
   @import "../style/font-awesome-4.7.0/css/font-awesome.min.css";
 
+
   #picture-display{
-    background-color: white;
+    height: 441px;
+    width: 508px;
     display: inline-block;
     padding: 7px;
+    position: relative;
+    vertical-align: top;
+    padding: 5px 5px 66px;
+    margin-left: 0px;
+    margin-bottom: 0px;
+  }
+
+  .post-item{
+    position: absolute;
+    left: 5px;
+    right: 5px;
+    top: 5px;
+    bottom: 65px;
+    background-color: #ffffff;
+  }
+
+  .post-body{
+    position: absolute;
+    left: 6px;
+    top:6px;
+    right: 6px;
+    bottom: 0px;
   }
 
   .image{
     width: 100%;
+    height: 100%;
     display: block;
     cursor:pointer;
+    background-color: rgb(160, 161, 154);
+    background-size: cover;
+    background-position: center;
   }
 
-  .image:hover{
-    -moz-opacity:0.7;
-    opacity: 0.7;
-    filter: alpha(Opacity=70);
-    background: #222222;
+  .image-mask{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    /*background-color: #000000;*/
+    color: #fff;
+    transition: opacity .2s;
+    opacity: 0;
+    cursor: pointer ;
+    background: #000000; /* For browsers that do not support gradients */
+    background: -webkit-radial-gradient(circle, #444444, #222222, #000000); /* Safari */
+    background: -o-radial-gradient(circle, #444444, #222222, #000000); /* Opera 11.6 to 12.0 */
+    background: -moz-radial-gradient(circle, #444444, #222222, #000000); /* Firefox 3.6 to 15 */
+    background: radial-gradient(circle, #444444, #222222, #000000); /* Standard syntax */
+  }
+
+  .image-mask:hover{
+    opacity: 0.5;
   }
 
   #picture-display-bottom{
+    position: absolute;
+    bottom: 5px;
+    left: 5px;
+    right: 5px;
+    height: 60px;
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
     height: 60px;
+    background-color: white;
+    font-size: 15px;
+    color: #222222;
   }
 
   /*#picture-display .button {*/
@@ -64,16 +137,12 @@
     display: block;
   }
 
-  #picture-display-bottom{
-    font-size: 15px;
-    color: #222222;
-  }
-
   #picture-display-bottom img{
     width: 40px;
     height: 40px;
     border-radius: 50%;
     padding-right: 3px;
+    padding-left: 5px;
     cursor: pointer;
   }
 
@@ -83,6 +152,7 @@
     justify-content: flex-end;
     align-items: center;
     flex-grow: 1;
+    padding-right: 5px;
   }
 
   #picture-display-button img{
@@ -101,5 +171,9 @@
   .picture-icon-num{
     color: #222222;
     margin-left: 5px;
+  }
+
+  .redColor{
+    color: #e60905;
   }
 </style>
